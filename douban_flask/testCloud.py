72a0fb0b21155958e064d6faa7a66b2306f785ca
pattonchow <40862752+pattonchow@users.noreply.github.com>
2022-08-10 16:35:59 -0400

    Add files via upload

---
+++ douban_flask/testCloud.py
@@ -0,0 +1,9 @@
+# -*- codeing = utf-8 -*-
+# @Time :2021/8/13 20:04
+# @Author: PATTON
+# @File : testCloud.py
+# @Software: PyCharm
+
+import jieba
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud
